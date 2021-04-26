c116875fd5fbae327df467d20659da973ac996e2
Lolozen <loris.pistilli.pro@gmail.com>
2021-04-26 15:23:30 +0200

    addinng compressed gif if converted gif size is higher than original file size

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import platform
 import sys
 from os import rename
 from pathlib import Path
-from moviepy.editor import VideoFileClip
 import filetype
 import requests
+from pygifsicle import optimize
 
 import convert
 from convert import convertFile
+
 try:
     from winreg import *
 except:
@@ -61,14 +62,16 @@ def checkfolder(path):
         guess = filetype.guess(fullpath)
         if guess is None:
             print(f'\033[93m\033[1mcouldn\'t process {path}/{file} you ended up with a binary file')
-        elif file[len(file)-len(guess.extension)-1:] != '.'+guess.extension:
+        elif file[len(file) - len(guess.extension) - 1:] != '.' + guess.extension:
             rename(fullpath, fullpath + '.' + guess.extension)
             if not guess.mime.split('/')[0] == 'video':
                 print(f'renamed {path}/{file} ➡ {path}/{file}.{guess.extension}')
             else:
-                convertFile(f'{fullpath}.{guess.extension}',convert.TargetFormat.GIF)
+                convertFile(f'{fullpath}.{guess.extension}', convert.TargetFormat.GIF)
+                if os.path.getsize(f'{fullpath}.{guess.extension}') < os.path.getsize(f'{fullpath}.gif'):
+                    print(f'optimizing {fullpath}.gif')
+                    optimize(f'{fullpath}.gif')
                 os.remove(f'{fullpath}.{guess.extension}')
-
 
 
 def finddownloaddirectory():
@@ -87,7 +90,7 @@ def finddownloaddirectory():
 
 
 def main(giffavs, folder):
-    errorcount = 0
+    errorlist = []
     value = json.loads(giffavs)["_state"]["favorites"]
     print(f'found {len(value)} faved gifs')
     for count, el in enumerate(value):
@@ -96,14 +99,15 @@ def main(giffavs, folder):
         print(f'\n➡{count + 1}: {url}')
         name = url.split('/')[-1].split('?')[-2] if '?' in url.split('/')[-1] else url.split('/')[-1]
         if not downloadurl(url, count, name, folder):
-            print(f'\t\033[93m\033[1mCouldn\'t download {name}\n\ttrying src method but you might end up with a non gif file :\033[0m')
+            print(
+                f'\t\033[93m\033[1mCouldn\'t download {name}\n\ttrying src method but you might end up with a non gif file :\033[0m')
             if not downloadsrc(src, count, name, folder):
                 print(
                     f'\t\033[91m\033[1mAn error occurred while downloading {name} try manually at source:\033[0m {el["src"]}')
-                errorcount += 1
-    if errorcount > 0:
+                errorlist.append(el["src"])
+    if len(errorlist) > 0:
         checkfolder(folder)
-    return errorcount
+    return errorlist
 
 
 if __name__ == '__main__':
@@ -112,7 +116,7 @@ if __name__ == '__main__':
     # put your JSON GIFFavorite store here
     data = ''
     #
-    
+
     folder = finddownloaddirectory()
 
     folder += '/DiscordFavoriteGif(' + now.strftime("%d") + '.' + now.strftime("%b") + '.' + now.strftime(
@@ -122,4 +126,4 @@ if __name__ == '__main__':
 
     result = main(data, folder)
 
-    print('script ended with ' + str(result) + ' error(s)')
+    print('script ended with ' + str(len(result)) + ' error(s)')
